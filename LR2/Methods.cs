37cebc147e773a5e5ee8680f6626f1b29990ac69
Ars-Nikon <80975576+Ars-Nikon@users.noreply.github.com>
2021-11-20 22:28:37 +0300

    Сделанная 2 лаба (можно и первую защитать)

---
+++ LR2/Methods.cs
@@ -0,0 +1,197 @@
+﻿using ConsoleTables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    static internal class Methods
+    {
+        static private int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+        static public void UniformSearch(int a, int b, int n, Func<double, double> Funct)
+        {
+
+            var fxi = new Dictionary<int, double>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                int xi = a + i * (b - a) / (n + 1);
+                var F = Funct(xi);
+                if (!double.IsNaN(F))
+                {
+                    fxi.Add(xi, Funct(xi));
+                }
+            }
+
+            var Min = fxi.
+                Where(x => x.Value == fxi.Min(y => y.Value)).
+                ToList();
+
+            var Max = fxi.
+                Where(x => x.Value == fxi.Max(y => y.Value)).
+                ToList();
+
+            Console.WriteLine("Метод равномерного поиска");
+            Console.WriteLine($"Iterations= {n}");
+
+            Console.WriteLine("Min:");
+            var table = new ConsoleTable("F(x)", "x");
+            Min?.ForEach(x => table.AddRow(x.Value, x.Key));
+            table.Write(Format.Alternative);
+
+            Console.WriteLine("Max:");
+            table = new ConsoleTable("F(x)", "x");
+            Max?.ForEach(x => table.AddRow(x.Value, x.Key));
+            table.Write(Format.Alternative);
+        }
+
+        static public void Dichotomies(double a, double b, double e, double delt, Func<double, double> Funct)
+        {
+            int i = 0;
+            while ((b - a) / 2 > e)
+            {
+                double x1 = (a + b - delt) / 2;
+                double x2 = (a + b + delt) / 2;
+                if (x1 < x2)
+                {
+                    double t = x1;
+                    x1 = x2;
+                    x2 = t;
+
+                }
+
+                var Fx1 = Funct(x1);
+                var Fx2 = Funct(x2);
+                if (double.IsNaN(Fx1) || double.IsNaN(Fx2))
+                {
+                    continue;
+                }
+
+                if (Fx1 <= Fx2)
+                {
+                    b = x2;
+                }
+                else
+                {
+                    a = x1;
+                }
+                i++;
+            }
+
+            Console.WriteLine("Метод дихотомии");
+            Console.WriteLine($"Iterations= {i}");
+            var table = new ConsoleTable("F(x)", "x");
+            table.AddRow(Funct((a + b) / 2), (a + b) / 2);
+            table.Write(Format.Alternative);
+        }
+
+        static public void GoldenSection(double a, double b, double e, Func<double, double> Funct)
+        {
+
+            Console.WriteLine("Метод золотого сечения");
+            Console.WriteLine("Min:");
+            GoldenSectionHelper(a, b, e, Funct, (x, y) => x >= y);
+            Console.WriteLine("Max:");
+            GoldenSectionHelper(a, b, e, Funct, (x, y) => x <= y);
+        }
+
+        static private void GoldenSectionHelper(double a, double b, double e, Func<double, double> Funct, Func<double, double, bool> action)
+        {
+            double PHI = (1 + Math.Sqrt(5)) / 2;
+
+            double x1;
+            double x2;
+            var i = 0;
+            while (true)
+            {
+                x1 = b - (b - a) / PHI;
+                x2 = a + (b - a) / PHI;
+                if (action(Funct(x1), Funct(x2)))
+                {
+                    a = x1;
+                }
+                else
+                {
+                    b = x2;
+                }
+                if (Math.Abs(b - a) < e)
+                    break;
+                i++;
+            }
+
+            Console.WriteLine($"Iterations= {i}");
+            var table = new ConsoleTable("F(x)", "x");
+            table.AddRow(Funct((a + b) / 2), (a + b) / 2);
+            table.Write(Format.Alternative);
+        }
+
+        static public void Fibon(double a, double b, double e, Func<double, double> Funct)
+        {
+            Console.WriteLine("Метод фибоначчи");
+            Console.WriteLine("Min:");
+            FibonHelper(a, b, e, Funct, (x, y) => x > y);
+            Console.WriteLine("Max:");
+            FibonHelper(a, b, e, Funct, (x, y) => x < y);
+        }
+
+        static private void FibonHelper(double a, double b, double e, Func<double, double> Funct, Func<double, double, bool> action)
+        {
+            int n = (int)((b - a) / e);
+            int k = 0;
+
+            for (int i = 1; ; i++)
+            {
+                if (Fibonachi(i) < n)
+                {
+                    continue;
+                }
+                k = i;
+                break;
+            }
+
+            double l = (b - a) / Fibonachi(k);
+
+            double x1, x2;
+            double Fx1, Fx2;
+
+
+            for (int i = k - 2; i != 0; i--)
+            {
+                x1 = a + l * Fibonachi(i);
+                x2 = b - l * Fibonachi(i);
+
+
+                Fx1 = Funct(x1);
+                Fx2 = Funct(x2);
+
+                if (double.IsNaN(Fx1) || double.IsNaN(Fx2))
+                {
+                    continue;
+                }
+
+                if (action(Fx1,Fx2))
+                {
+                    b = x2;
+                }
+                else
+                {
+                    a = x1;
+                }
+            }
+
+            Console.WriteLine($"Iterations= {k - 2}");
+            var table = new ConsoleTable("F(x)", "x");
+            table.AddRow(Funct((a + b) / 2), (a + b) / 2);
+            table.Write(Format.Alternative);
+        }
+
+
+    }
+}
--- LR2/Program.cs
+++ LR2/Program.cs
@@ -8,113 +8,31 @@ namespace LR2
     {
         static double Funct(double x)
         {
-             return Math.Pow((25 - Math.Pow(x, 2)), 0.5);
-            // return Math.Pow(Math.Pow((Math.Pow(x,2)-1),2),1/3);
+            // return Math.Pow((25 - Math.Pow(x, 2)), 0.5);
+            //  return Math.Pow(Math.Pow((Math.Pow(x,2)-1),2),1/3);
+            // return Math.Pow(x, 4) + Math.Exp(-x);
+            return - 2 * Math.Pow(x, 2) + 2 * x + 2;
         }
         static double DerFunct(double x)
         {
             return -(x / Math.Sqrt(25 - Math.Pow(x, 2)));
         }
+        static int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1) return n;
 
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
 
         static void Main(string[] args)
         {
+            Methods.UniformSearch(-6, 6, 1, Funct);
 
-            //  унимодальна на отрезке   Метод равномерного поиска
-            /*
-          int a = -6;
-            int b = 6;
-            int  e = 1;
+            Methods.Dichotomies(-6, 6, 0.1, 0.001, Funct);
 
-           int n = (b - a) / e - 1;
+            Methods.GoldenSection(-6, 6, 0.001, Funct);
 
-           var fxi = new Dictionary<int, double>();
-
-           for (int i = 1; i <= n; i++)
-           {
-               int xi = a + i * (b - a) / (n + 1);
-               fxi.Add(xi, Funct(xi));
-           }
-
-           var Min = fxi.Where(x => x.Value == fxi.Min(y => y.Value)).ToList();
-           var Max = fxi.Where(x => x.Value == fxi.Max(y => y.Value)).ToList();
-           */
-
-
-            //Метод дихотомии
-            /*
-            double a = -6;
-            double b = 6;
-            double e = 0.1;
-            double delt = 0.02;
-
-
-            while ((b - a) / 2 > e)
-            {
-                double x1 = (a + b - delt) / 2;
-                double x2 = (a + b + delt) / 2;
-                if (x1 < x2)
-                {
-                    double t = x1;
-                    x1 = x2;
-                    x2 = t;
-
-                }
-
-                var Fx1 = Funct(x1);
-                var Fx2 = Funct(x2);
-
-                if (Fx1 <= Fx2)
-                {
-                    b = x2;
-                }
-                else
-                {
-                    a = x1;
-                }
-            }
-
-            var F = Funct((a + b) / 2);
-            */
-
-
-            // Метод золотого сечения.
-            /*
-            double a = -6;
-            double b = 6;
-            double e = 0.1;
-            double en = Math.Abs(b-a);
-            double t = (Math.Sqrt(5) + 1)/2;
-
-            double x1 = a + (b - a) / (t + 1);
-            double x2 = b - (b - a) / (t + 1); ;
-            var Fx1 = Funct(x1);
-            var Fx2 = Funct(x2);
-
-            while (en<e)
-            {
-                if (Fx1 < Fx2)
-                {
-                    b = x2;
-                    x2 = x1;
-                    Fx2 = Fx1;
-                    x1 = a + (b - a) / (t + 1);
-                    Fx1 = Funct(x1);
-                }
-                else
-                {
-                    a = x1;
-                    x1 = x2;
-                    Fx1 = Fx2;
-                    x2= b - (b - a) / (t + 1);
-                    Fx2 = Funct(x2);
-                }
-            }
-
-            var F = Funct((a + b) / 2);
-            Console.WriteLine();
-            */
-
+            Methods.Fibon(-6, 6, 0.01, Funct);
         }
     }
 }
